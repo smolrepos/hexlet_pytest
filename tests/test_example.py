ef4877364fda1deb59dc3a342854e4f0e6184d61
Иван <ivan@iMac-Ivan.local>
2023-08-17 15:13:01 +0300

    lesson4

---
+++ tests/test_example.py
@@ -0,0 +1,10 @@
+from example import reverse
+
+def test_reverse():
+    assert reverse('Hexlet') == 'telxeH'
+
+def test_reverse_for_empty_string():
+    assert reverse('') == ''
+
+
+print('All tests completed!')
